1fb22862f72076082a10546b26e45964f398a599
Anthony Crowcroft <anthonycrowcroft@gmail.com>
2016-09-27 10:55:07 +1300

    SEO check

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Anthony Paul Crowcroft</title>
+    <meta name="google-site-verification" content="nLayH0ppVcnTipa7tzDaXVWmUkOsAbsvPIrjhZdPCAM" />
 
     <base href="/">
 
